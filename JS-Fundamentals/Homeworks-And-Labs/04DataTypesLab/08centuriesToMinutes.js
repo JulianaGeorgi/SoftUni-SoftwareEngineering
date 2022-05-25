5285067a4a66f4f02765a67f907670141d89531c
JulianaGeorgi <dzhuliyana.georgieva@gmail.com>
2022-05-25 18:24:46 +0100

    Did exercises

--- JS-Fundamentals/Homeworks-And-Labs/04DataTypesLab/08centuriesToMinutes.js
+++ JS-Fundamentals/Homeworks-And-Labs/04DataTypesLab/08centuriesToMinutes.js
@@ -5,7 +5,7 @@ function centuriesToMinutes(centuries){
     let hours = days * 24;
     let minutes = hours * 60;
 
-    console.log(`${centuries} centuries = ${years}`+` years = ${days} `+ `days = ${hours} `+` hours = ${minutes} minutes`);
+    console.log(`${centuries} centuries = ${years}`+` years = ${days} `+ `days = ${hours} `+`hours = ${minutes} minutes`);
 }
 
 centuriesToMinutes(1);
---
+++ JS-Fundamentals/Homeworks-And-Labs/04DataTypesLab/09specialNumbers.js
@@ -0,0 +1,30 @@
+function specialNumbers(n) {
+
+    for (i = 1; i <= n; i++) {
+
+        if (i > 9) {
+
+            i = i.toString();
+            let sum = 0;
+
+            for (let i2 = 0; i2 < i.length; i2++) {
+                sum += Number(i[i2]);
+            }
+
+            if(sum === 5 || sum === 7 || sum === 11){
+                console.log(`${i} -> True`);
+            } else {
+                console.log(`${i} -> False`);
+            }
+
+        } else if (i === 5 || i === 7) {
+            console.log(`${i} -> True`);
+
+        } else {
+            console.log(`${i} -> False`);
+        }
+    }
+
+}
+
+specialNumbers(20);
