ac8fc1041f57447087f8e96a7f1552ecd02d2b41
JulianaGeorgi <dzhuliyana.georgieva@gmail.com>
2022-06-12 18:22:31 +0100

    Added tasks

--- JS-Fundamentals/Homeworks-And-Labs/10FunctionsLab/06signCheck.js
+++ JS-Fundamentals/Homeworks-And-Labs/10FunctionsLab/06signCheck.js
@@ -1,5 +1,23 @@
-function signCheck(){
+function signCheck(num1, num2, num3) {
+
+    let arrayFromNumbers = [num1, num2, num3];
+    let negative = 0;
+
+    for (let el of arrayFromNumbers) {
+        if (el < 0) {
+            negative++;
+        }
+    }
+
+    if (negative % 2 === 0) {
+        console.log("Positive");
+    } else {
+        console.log("Negative");
+    }
 
 }
 
-signCheck();
+signCheck(5, 12, -15);
+signCheck(-6, -12, 14);
+signCheck(-1, -2, -3);
+signCheck(-5, 1, 1);
---
+++ JS-Fundamentals/Homeworks-And-Labs/11FunctionsExercises/09loadingBar.js
@@ -0,0 +1,18 @@
+function main(number) {
+
+    let completed = '%'.repeat(number / 10);
+    let incomplete = '.'.repeat(10 - completed.length);
+
+    if (incomplete.length === 0) {
+        console.log('100% Complete!');
+
+    } else {
+
+        console.log(`${number}% [${completed}${incomplete}]`);
+        console.log('Still loading...');
+    }
+
+    console.log();
+}
+
+main(30);
