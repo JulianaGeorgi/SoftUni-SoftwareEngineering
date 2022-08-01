e170e8e027b064599d74dd5e6ebb76cf154e4c4e
JulianaGeorgi <dzhuliyana.georgieva@gmail.com>
2022-08-01 09:29:12 +0100

    Renamed folder

---
+++ JS-Fundamentals/Other-Exercises/findBiggestDifference.js
@@ -0,0 +1,7 @@
+// find the biggest difference between a number in the first array and a number in the second array
+
+function findBiggestDifference(first, second){
+
+}
+
+findBiggestDifference([1, 'asd', '1', {}, '1a00', 5, 1, 3],[10, 22, 'a', false]);
