b9ed3125c05a00c2cfaf140aee2763dabdf4b16c
JulianaGeorgi <dzhuliyana.georgieva@gmail.com>
2022-08-08 20:15:08 +0100

    Added a task

---
+++ JS-Fundamentals/Homeworks-And-Labs/21TextProcessingExercise/01revealWords.js
@@ -0,0 +1,15 @@
+function revealWords(words, sentence) {
+
+    let wordsArr = words.split(", ");
+
+    for (let word of wordsArr) {
+
+        let template = "*".repeat(word.length);
+        sentence = sentence.replace(template, word);
+    }
+
+    console.log(sentence);
+}
+
+revealWords('great',
+    'softuni is ***** place for learning new programming languages');
