d05a1818aed36329768b9a7cbef6d26cb15c57da
JulianaGeorgi <dzhuliyana.georgieva@gmail.com>
2022-08-12 15:31:39 +0100

    Added a task

---
+++ JS-Fundamentals/Homeworks-And-Labs/21TextProcessingExercise/05replaceRepeatingChars.js
@@ -0,0 +1,14 @@
+function replaceRepeatingChars(input) {
+
+    let letters = '';
+
+    for (let el of input) {
+
+        if (el !== letters[letters.length - 1]) {
+            letters += el;
+        }
+    }
+    console.log(letters);
+}
+
+replaceRepeatingChars('aaaaabbbbbcdddeeeedssaa');
