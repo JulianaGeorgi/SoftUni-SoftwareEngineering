fb6bc2e87cf2d4ad45e3b64ecf0f3db86cda1717
JulianaGeorgi <dzhuliyana.georgieva@gmail.com>
2022-09-15 10:27:21 +0100

    Renamed folder

--- JS-Advanced/Homeworks-And-Labs/02Syntax-Functions-and-Statements-Exercise/06roadRadar.js
+++ JS-Advanced/Homeworks-And-Labs/02Syntax-Functions-and-Statements-Exercise/06roadRadar.js
@@ -10,19 +10,21 @@ function roadRadar(speed, roadType) {
 
     let status = ``;
     let difference = speed - roadTypes[roadType];
-    
+
     if (speed > roadTypes[roadType]) {
-        if (difference <= 20) status = `speeding`;
-        else if (difference <= 40) status = `excessive speeding`;
-        else status = `reckless driving`;
+        if (difference <= 20) {
+            status = 'speeding';
+        }
+        else if (difference <= 40) {
+            status = 'excessive speeding';
+        } else {
+            status = 'reckless driving';
+        }
     }
+
     speed > roadTypes[roadType]
-        ? console.log(
-            `The speed is ${difference} km/h faster than the allowed speed of ${roadTypes[roadType]} - ${status}`
-        )
+        ? console.log(`The speed is ${difference} km/h faster than the allowed speed of ${roadTypes[roadType]} - ${status}`)
         : console.log(`Driving ${speed} km/h in a ${roadTypes[roadType]} zone`);
-
-
 }
 
 roadRadar(40, 'city');
