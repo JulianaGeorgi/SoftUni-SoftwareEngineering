e88252be15db7def3ab7938e29a2c9027e2f4082
JulianaGeorgi <dzhuliyana.georgieva@gmail.com>
2022-09-20 16:46:08 +0100

    Added tasks

---
+++ JS-Advanced/Homeworks-And-Labs/06Objects-And-Composition-Exercise/01caloriesObject.js
@@ -0,0 +1,11 @@
+function calorieObject(data) {
+
+    let res = {};
+    for (let i = 0; i < data.length; i += 2) { // to take them in couples 
+        res[data[i]] = Number(data[i + 1]);
+    }
+    console.log(res);
+}
+
+calorieObject(['Yoghurt', '48', 'Rise', '138', 'Apple', '52']);
+calorieObject(['Potato', '93', 'Skyr', '63', 'Cucumber', '18', 'Milk', '42']);
