3e2e3e4ac3c253a8fba606defb761d4e29af8fa3
JulianaGeorgi <dzhuliyana.georgieva@gmail.com>
2022-09-27 12:29:53 +0100

    Added tasks

---
+++ JS-Advanced/Homeworks-And-Labs/07DOMIntroduction-Lab/04.Show More/ShowMore.js
@@ -0,0 +1,8 @@
+function showText() {
+    
+    let smallText = document.getElementById('more');
+    let fullText = document.getElementById('text');
+
+    smallText.style.display = 'none';
+    fullText.style.display = 'inline';
+}
