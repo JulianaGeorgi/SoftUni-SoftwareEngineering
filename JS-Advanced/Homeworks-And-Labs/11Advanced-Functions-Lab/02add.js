3bf2e6727ab01208568167c7a1391b8bd7f4b560
JulianaGeorgi <dzhuliyana.georgieva@gmail.com>
2022-09-30 10:03:38 +0100

    Added tasks

---
+++ JS-Advanced/Homeworks-And-Labs/11Advanced-Functions-Lab/02add.js
@@ -0,0 +1,15 @@
+function solution(num) {
+    return function sum(secondNum) {
+        return num + secondNum;
+    }
+}
+
+let add5 = solution(5);
+console.log(add5(2));
+console.log(add5(3));
+
+console.log("---------------");
+
+let add7 = solution(7);
+console.log(add7(2));
+console.log(add7(3));
