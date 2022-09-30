b5001268b9363e6be0004ca6c72448fff33f5316
JulianaGeorgi <dzhuliyana.georgieva@gmail.com>
2022-09-30 08:29:07 +0100

    Added task

---
+++ JS-Advanced/Homeworks-And-Labs/11Advanced-Functions-Lab/05commandProcessor.js
@@ -0,0 +1,27 @@
+function commandProcessor() {
+    let str = '';
+    return {
+        append: (s) => str += s,
+        removeStart: (n) => str = str.substring(n),
+        removeEnd: (n) => str = str.substring(0, str.length - n),
+        print: () => console.log(str)
+    }
+}
+
+let firstZeroTest = commandProcessor();
+
+firstZeroTest.append('hello');
+firstZeroTest.append('again');
+firstZeroTest.removeStart(3);
+firstZeroTest.removeEnd(4);
+firstZeroTest.print();
+
+console.log("------------------");
+
+let secondZeroTest = commandProcessor();
+
+secondZeroTest.append('123');
+secondZeroTest.append('45');
+secondZeroTest.removeStart(2);
+secondZeroTest.removeEnd(1);
+secondZeroTest.print();
