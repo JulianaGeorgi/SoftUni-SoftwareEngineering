774f93c7790c0495a99e190fb052b066dabea6f3
JulianaGeorgi <dzhuliyana.georgieva@gmail.com>
2022-11-02 18:19:50 +0000

    Cosmetic changes

--- JS-Applications/04Data-And-Authentication-Exercise/01.Messenger/app.js
+++ JS-Applications/04Data-And-Authentication-Exercise/01.Messenger/app.js
@@ -1,50 +1,48 @@
+const textArea = document.getElementById("messages");
+const nameField = document.querySelector('[name="author"]');
+const contentField = document.querySelector('[name="content"]');
 
 attachEvents();
 
 function attachEvents() {
-    
-    document.getElementById("refresh").addEventListener("click", async ()=>{
+    document.getElementById("refresh").addEventListener("click", async () => {
         const messages = await getMessages();
         displayMessages(messages);
     })
     document.getElementById("submit").addEventListener("click", onSend);
 }
 //getting the value from the input fields and creating the message object to pass to postMessage
-async function onSend(){
-    const nameField = document.querySelector('[name="author"]');
-    const contentField = document.querySelector('[name="content"]');
-    await postMessage({author: nameField.value, content: contentField.value});
+async function onSend() {
+    await postMessage({ author: nameField.value, content: contentField.value });
     nameField.value = "";
-    contentField.value ="";
+    contentField.value = "";
 }
-//displaying all existing messages in the comments area
-function displayMessages(messages){
-    const textArea = document.getElementById("messages");
-    textArea.value ="";
+//displaying all existing messages in the  textarea
+function displayMessages(messages) {
+    textArea.value = "";
     const result = [];
-    [...messages].forEach(message => {
+    messages.forEach(message => {
         const messageToAppend = `${message.author}: ${message.content}`;
         result.push(messageToAppend);
     });
-
     textArea.value = result.join('\n');
+    debugger;
 }
-//reading the comments 
-async function getMessages(){
+//reading the messages 
+async function getMessages() {
     const response = await fetch("http://localhost:3030/jsonstore/messenger");
     const data = await response.json();
     return Object.values(data);
 }
-//creating the comments
-async function postMessage(message){
+//creating the messages
+async function postMessage(message) {
     const response = await fetch("http://localhost:3030/jsonstore/messenger", {
-        method: "post", 
+        method: "post",
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(message)
     });
-
     const data = await response.json();
     return data;
 }
