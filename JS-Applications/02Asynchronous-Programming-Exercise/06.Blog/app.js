3e472910fac9af8024a169cfffa7eb40b2f54d50
JulianaGeorgi <dzhuliyana.georgieva@gmail.com>
2022-11-03 09:19:36 +0000

    Made cosmetic changes

--- JS-Applications/02Asynchronous-Programming-Exercise/06.Blog/app.js
+++ JS-Applications/02Asynchronous-Programming-Exercise/06.Blog/app.js
@@ -1,7 +1,6 @@
 function attachEvents() {
 
-    const loadPostsBtn = document.getElementById("btnLoadPosts");
-    loadPostsBtn.addEventListener("click", loadPosts);
+    document.getElementById("btnLoadPosts").addEventListener("click", getPosts);
 
     const viewPostBtn = document.getElementById("btnViewPost");
     const select = document.getElementById("posts");
@@ -9,24 +8,26 @@ function attachEvents() {
     const postContent = document.getElementById("post-body");
     const postComments = document.getElementById("post-comments");
 
-    async function loadPosts() {
+    async function getPosts() {
         try {
             const response = await fetch('http://localhost:3030/jsonstore/blog/posts');
             const postsData = await response.json(); // receiving an object of objects
 
+            // // adding each post to the dropdown menu
             for (let key in postsData) {
                 const option = document.createElement('option');
-                option.value = key; // using the key of each object from the parent object 
+                option.value = key; // using the key of each object from the parent object 	
                 option.textContent = postsData[key].title; // using the current object title property as text inside the node 
                 select.appendChild(option);
             }
-            viewPostBtn.addEventListener("click", () => viewPost(postsData));
+            viewPostBtn.addEventListener("click", () => getComments(postsData));
+
         } catch (error) {
             console.log(error.message);
         }
     }
 
-    async function viewPost(postsData) {
+    async function getComments(postsData) {
         try {
             const selectedPostID = select.value;
             const selectedPostTitle = select.options[select.selectedIndex].text;
@@ -36,11 +37,9 @@ function attachEvents() {
             const allComments = await response.json();
 
             // need to match the selectedPostID with postID of the comments 
-
             const selectedPostComments = Object
                 .values(allComments)
                 .filter(obj => selectedPostID === obj.postId);
-            console.log(selectedPostComments);
 
             postTitle.textContent = selectedPostTitle;
             postContent.textContent = selectedPostContent;
