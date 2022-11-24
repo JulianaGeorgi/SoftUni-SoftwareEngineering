1e32c2d209cd1a515769309d4c0ab77eabd378b3
JulianaGeorgi <dzhuliyana.georgieva@gmail.com>
2022-11-24 20:22:42 +0000

    Uploaded final code

--- JS-Applications/12Exam-Prep/src/api/data.js
+++ JS-Applications/12Exam-Prep/src/api/data.js
@@ -1,6 +1,6 @@
 // responsible for the collection of data (pets)
 
-import { del, get } from './api.js';
+import { del, get, post, put } from './api.js';
 
 export async function getAllPets(){
     return get('/data/pets?sortBy=_createdOn%20desc&distinct=name');
@@ -12,4 +12,12 @@ export async function getById(id){
 
 export async function deleteById(id){
     return del('/data/pets/' + id);
+}
+
+export async function createPet(petData){
+    return post('/data/pets', petData);
+}
+
+export async function editPet(id, petData){
+    return put('/data/pets/' + id, petData);
 }
