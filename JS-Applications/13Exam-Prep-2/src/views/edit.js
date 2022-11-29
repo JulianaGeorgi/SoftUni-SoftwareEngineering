3d5dd9da5a01fefec8236b37c0bb91a37794b070
JulianaGeorgi <dzhuliyana.georgieva@gmail.com>
2022-11-29 12:00:01 +0000

    Fixed a mistake in the html

--- JS-Applications/13Exam-Prep-2/src/views/edit.js
+++ JS-Applications/13Exam-Prep-2/src/views/edit.js
@@ -7,12 +7,12 @@ const editTemp = (shoe, onEdit) => html`
     <div class="form">
         <h2>Edit item</h2>
         <form @submit=${onEdit} class="edit-form">
-            <input type="text" name="brand" id="shoe-brand" .placeholder=${shoe.brand} />
-            <input type="text" name="model" id="shoe-model" .placeholder=${shoe.model} />
-            <input type="text" name="imageUrl" id="shoe-img" .placeholder=${shoe.imageUrl} />
-            <input type="text" name="release" id="shoe-release" .placeholder=${shoe.release} />
-            <input type="text" name="designer" id="shoe-designer" .placeholder=${shoe.designer} />
-            <input type="text" name="value" id="shoe-value" .placeholder=${shoe.value} />
+            <input type="text" name="brand" id="shoe-brand" .value=${shoe.brand} />
+            <input type="text" name="model" id="shoe-model" .value=${shoe.model} />
+            <input type="text" name="imageUrl" id="shoe-img" .value=${shoe.imageUrl} />
+            <input type="text" name="release" id="shoe-release" .value=${shoe.release} />
+            <input type="text" name="designer" id="shoe-designer" .value=${shoe.designer} />
+            <input type="text" name="value" id="shoe-value" .value=${shoe.value} />
 
             <button type="submit">post</button>
         </form>
