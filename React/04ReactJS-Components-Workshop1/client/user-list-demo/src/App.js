caacd91899acab224b369f4a061cbef7955040d0
JulianaGeorgi <dzhuliyana.georgieva@gmail.com>
2023-03-24 14:48:49 +0000

    Created main components

--- React/04ReactJS-Components-Workshop1/client/user-list-demo/src/App.js
+++ React/04ReactJS-Components-Workshop1/client/user-list-demo/src/App.js
@@ -1,5 +1,25 @@
+import { Fragment } from "react";
+import { Footer } from "./components/Footer.js";
+import { Header } from "./components/Header.js";
+import { Search } from "./components/Search.js";
+import './App.css';
+import { UserList } from "./components/UserList.js";
+
 function App() {
-  return;
+  return (
+    <Fragment>
+
+      <Header />
+      <main className="main">
+        <section class="card users-container">
+          <Search />
+          <UserList/>
+        </section>
+      </main>
+      <Footer />
+
+    </Fragment>
+  );
 }
 
 export default App;
