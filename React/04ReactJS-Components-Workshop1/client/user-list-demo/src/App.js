6b0ed879e545a0004e95b4f3486ff1c55cb8722f
JulianaGeorgi <dzhuliyana.georgieva@gmail.com>
2023-03-25 15:55:05 +0000

    Display users from db

--- React/04ReactJS-Components-Workshop1/client/user-list-demo/src/App.js
+++ React/04ReactJS-Components-Workshop1/client/user-list-demo/src/App.js
@@ -1,4 +1,7 @@
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
+
+import * as userService from './services/userService';
+
 import { Footer } from "./components/Footer.js";
 import { Header } from "./components/Header.js";
 import { Search } from "./components/Search.js";
@@ -6,14 +9,25 @@ import './App.css';
 import { UserList } from "./components/UserList.js";
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    userService.getAll()
+      .then(setUsers)
+      .catch(err => {
+        console.log('Error' + err);
+      });
+  }, []);
+
   return (
     <Fragment>
 
       <Header />
       <main className="main">
-        <section class="card users-container">
+        <section className="card users-container">
           <Search />
-          <UserList/>
+          <UserList users = {users} />
         </section>
       </main>
       <Footer />
