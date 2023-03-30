dc37af3a8fb1c30cc6ee16aa3db321d813ae77ea
JulianaGeorgi <dzhuliyana.georgieva@gmail.com>
2023-03-30 16:21:19 +0100

    Created the routing

--- React/06Routing/Workshop/games-play-demo/src/App.js
+++ React/06Routing/Workshop/games-play-demo/src/App.js
@@ -1,9 +1,10 @@
+import { Routes, Route } from 'react-router-dom';
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
 import { Home } from "./components/Home/Home";
 import { Login } from "./components/Login/Login";
 import { Register } from "./components/Register/Register";
-import { CreateGame} from "./components/CreateGame/CreateGame"
+import { CreateGame } from "./components/CreateGame/CreateGame"
 import { Catalog } from "./components/Catalog/Catalog.js";
 
 function App() {
@@ -12,11 +13,13 @@ function App() {
             <Header />
 
             <main id="main-content">
-            {/* <Home/> */}
-            {/* <Login/> */}
-            {/* <Register/> */}
-            {/* <CreateGame/> */}
-            <Catalog/>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/register' element={<Register />} />
+                    <Route path='/create-game' element={<CreateGame />} />
+                    <Route path='/catalog' element={<Catalog />} />
+                </Routes>
             </main>
 
             <Footer />
