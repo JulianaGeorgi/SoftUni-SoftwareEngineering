22b67c38488135f4049be09b80ebf438cd877a51
JulianaGeorgi <dzhuliyana.georgieva@gmail.com>
2023-04-12 14:35:33 +0100

    Changed class to className

--- React/React-Project/project-blog/src/Components/CreateBlog/CreateBlog.js
+++ React/React-Project/project-blog/src/Components/CreateBlog/CreateBlog.js
@@ -1,33 +1,33 @@
 export const CreateBlog = () => {
     return (
         <section id="contact" data-stellar-background-ratio="0.5">
-            <div class="container">
-                <div class="row">
+            <div className="container">
+                <div className="row">
 
-                    <div class="col-md-12 col-sm-12">
-                        <div class="section-title">
+                    <div className="col-md-12 col-sm-12">
+                        <div className="section-title">
                             <h2>Contact us</h2>
-                            <span class="line-bar">...</span>
+                            <span className="line-bar">...</span>
                         </div>
                     </div>
 
-                    <div class="col-md-8 col-sm-8">
+                    <div className="col-md-8 col-sm-8">
 
                         <form id="contact-form" action="#" method="post">
-                            <div class="col-md-6 col-sm-6">
-                                <input type="text" class="form-control" placeholder="Full Name" id="cf-name" name="cf-name" required="" />
+                            <div className="col-md-6 col-sm-6">
+                                <input type="text" className="form-control" placeholder="Full Name" id="cf-name" name="cf-name" required="" />
                             </div>
 
-                            <div class="col-md-6 col-sm-6">
-                                <input type="email" class="form-control" placeholder="Your Email" id="cf-email" name="cf-email" required="" />
+                            <div className="col-md-6 col-sm-6">
+                                <input type="email" className="form-control" placeholder="Your Email" id="cf-email" name="cf-email" required="" />
                             </div>
 
-                            <div class="col-md-6 col-sm-6">
-                                <input type="tel" class="form-control" placeholder="Your Phone" id="cf-number" name="cf-number" required="" />
+                            <div className="col-md-6 col-sm-6">
+                                <input type="tel" className="form-control" placeholder="Your Phone" id="cf-number" name="cf-number" required="" />
                             </div>
 
-                            <div class="col-md-6 col-sm-6">
-                                <select class="form-control" id="cf-budgets" name="cf-budgets">
+                            <div className="col-md-6 col-sm-6">
+                                <select className="form-control" id="cf-budgets" name="cf-budgets">
                                     <option>Budget Level</option>
                                     <option>$500 to $1,000</option>
                                     <option>$1,000 to $2,200</option>
@@ -38,12 +38,12 @@ export const CreateBlog = () => {
                                 </select>
                             </div>
 
-                            <div class="col-md-12 col-sm-12">
-                                <textarea class="form-control" rows="6" placeholder="Your requirements" id="cf-message" name="cf-message" required=""></textarea>
+                            <div className="col-md-12 col-sm-12">
+                                <textarea className="form-control" rows="6" placeholder="Your requirements" id="cf-message" name="cf-message" required=""></textarea>
                             </div>
 
-                            <div class="col-md-4 col-sm-12">
-                                <input type="submit" class="form-control" name="submit" value="Send Message" />
+                            <div className="col-md-4 col-sm-12">
+                                <input type="submit" className="form-control" name="submit" value="Send Message" />
                             </div>
 
                         </form>
