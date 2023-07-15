abc206c2bf53b1f7a287b0d360cb19c8d3033a0d
Juliana <106109538+JulianaGeorgi@users.noreply.github.com>
2023-07-15 13:24:24 +0100

    Added register form validations

--- Angular/Final-Project/travel-blog-app/src/app/user/register/register.component.ts
+++ Angular/Final-Project/travel-blog-app/src/app/user/register/register.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
-import { UserService } from '../user.service';
 import { Router } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms'
+import { appEmailValidator } from 'src/app/shared/validators/email.validator';
+import { DEFAULT_EMAIL_DOMAINS } from 'src/app/shared/constants';
+import { matchPasswordsValidator } from 'src/app/shared/validators/match.passwords.validator';
 
 @Component({
   selector: 'app-register',
@@ -8,14 +11,26 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  form = this.formBuilder.group({
+    email: ["", [Validators.required, Validators.minLength(5)]],
+    username: ["", [Validators.required, appEmailValidator(DEFAULT_EMAIL_DOMAINS)]],
+    phone: [""],
+    passGroup: this.formBuilder.group(
+      {
+        password: ["", [Validators.required, Validators.minLength(5)]],
+        rePassword: ["", [Validators.required]],
+      },
+      {
+        validators: [matchPasswordsValidator("password", "rePassword")],
+      }
+    ),
+  });
 
+  constructor(private formBuilder: FormBuilder, private router: Router) { }
 
-  constructor(private userService: UserService, private router: Router){
-
-  }
-
-  register(email: string, password:string, rePassword: string): void {
-    // this.userService.register();
-    // this.router.navigate(['/']);
+  register(): void {
+    if (this.form.invalid) {
+      return;
+    }
   }
 }
