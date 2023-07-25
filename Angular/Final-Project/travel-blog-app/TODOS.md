b5604d89b2c4d935da4c0e887e8afcd45a99c136
Juliana <106109538+JulianaGeorgi@users.noreply.github.com>
2023-07-25 15:21:19 +0100

    Edit tip from validation

--- Angular/Final-Project/travel-blog-app/TODOS.md
+++ Angular/Final-Project/travel-blog-app/TODOS.md
@@ -1,7 +1,7 @@
 1. State management
 2. Fix any types as much as possible 
 3. Check if all Observables are completed? Mark all Observables $?
-4. Add the update form validation 
+4. Add the update form validation  - DONE!!!
 5. Check the other forms 
 6. Improve/Edit the validation
 7. Fix /profile page -> needs to redirect to login when user is not logged in
--- Angular/Final-Project/travel-blog-app/src/app/travel-tip/update-tip/update-tip.component.ts
+++ Angular/Final-Project/travel-blog-app/src/app/travel-tip/update-tip/update-tip.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TipService } from '../tip.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { FormControl, FormGroup } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { FormBuilder, Validators } from '@angular/forms';
 import { MatSnackBarComponent } from 'src/app/shared/mat-snack-bar/mat-snack-bar.component';
 
 @Component({
@@ -11,12 +10,12 @@ import { MatSnackBarComponent } from 'src/app/shared/mat-snack-bar/mat-snack-bar
   styleUrls: ['./update-tip.component.css']
 })
 export class UpdateTipComponent implements OnInit {
-
-  postForm = new FormGroup({
-    tipTitle: new FormControl(''),
-    authorName: new FormControl(''),
-    imageUrl: new FormControl(''),
-    tipContent: new FormControl(''),
+  
+  tipForm = this.formBuilder.group({
+    tipTitle: ['', [Validators.required, Validators.minLength(3)]],
+    authorName: ['', [Validators.required]],
+    imageUrl: ['', [Validators.required]],
+    tipContent: ['', [Validators.required, Validators.minLength(10)]],
   });
 
   userId: string | null = this.route.snapshot.paramMap.get('userId');
@@ -24,7 +23,7 @@ export class UpdateTipComponent implements OnInit {
 
   tip$ = this.tipService.getOneTip(this.userId, this.tipId)
     .subscribe((tip) => {
-      this.postForm.setValue({
+      this.tipForm.setValue({
         tipTitle: tip.tipTitle,
         authorName: tip.authorName,
         imageUrl: tip.imageUrl,
@@ -36,12 +35,13 @@ export class UpdateTipComponent implements OnInit {
     private tipService: TipService,
     private route: ActivatedRoute,
     private router: Router,
+    private formBuilder: FormBuilder,
     private snackBar: MatSnackBarComponent
   ) { }
 
   onEditTip(): void {
 
-    const tipDetails = this.postForm.value;
+    const tipDetails = this.tipForm.value;
 
     this.tipService.editTip(tipDetails, this.userId, this.tipId);
 
@@ -50,7 +50,7 @@ export class UpdateTipComponent implements OnInit {
     const confirmMessage = "Tip edited successfully.";
     this.snackBar.openSnackBar(confirmMessage, 'Great!');
 
-    this.router.navigate([`traveltips/${this.userId}/${this.tipId}`]);
+    this.router.navigate([`traveltips/all/${this.userId}/${this.tipId}`]);
   };
 
   ngOnInit(): void {
