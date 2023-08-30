d33979e21d0aedee6d73c306d01e40bc780145d9
Juliana <106109538+JulianaGeorgi@users.noreply.github.com>
2023-08-30 20:57:45 +0100

    Firebase setup

--- React/Final-Project/greeny/src/services/auth.js
+++ React/Final-Project/greeny/src/services/auth.js
@@ -1,10 +1,16 @@
-export const registerUser = async (name, username, email, password) => {
-    // const data = {
-    //     name,
-    //     username,
-    //     email,
-    //     password
-    // };
-    // const response = await api.post(endpoint.register, data);
-    // return response;
-};
+import { auth } from "../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+export async function signup(email, password) {
+    try {
+        await createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+
+                const user = userCredential.user;
+
+                alert("Account successfully created :)");
+            })
+    } catch (err) {
+        alert(`Registration failed - ${err.code, err.message}`);
+    };
+}
